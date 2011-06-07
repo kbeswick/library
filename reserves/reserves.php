5007c35393e8d1e546444fd4829507087fdb56d0
Kevin Beswick <kevinbeswick00@gmail.com>
2011-06-07 14:23:48 -0400

    Cleanup of reserves.php -- we are using php 5.2, so take out 5.3 stuff. Will probably create a php5.3 branch or something later.

--- reserves/reserves.php
+++ reserves/reserves.php
@@ -1,31 +1,17 @@
 <?php
 
-// Configuration settings:
-
-$file_store = 'reserves.txt'; //name of place to store info
-
 // Take in values from a form and make a reserve
 function generate_reserve($bookbag_id, $course_code, $instructor)
 {
-	
-	//insert into db instead of flat file now
+	//use an sqlite db to store values
 	$db_location = 'reserves.db';
 
 	// Open sqlite db, then insert values
-	//PHP5.2 way
 	$db_session = new PDO('sqlite:' . $db_location);
 	$query = "INSERT into reserve (course_code, instructor, bookbag_id) values ('".$course_code."', '".$instructor."', ".$bookbag_id.")";
 	$results = $db_session->Query($query);
 	if (!$results)
 		die('Query Failed... Please go back and retry');
-/*
-	//PHP 5.3 way
-
-	$db_session = new SQLite3($db_location);
-	$query = "INSERT into reserve (course_code, instructor, bookbag_id) values ('".$course_code."', '".$instructor."', ".$bookbag_id.")";
-	$db_session->exec("INSERT into reserve (course_code, instructor, bookbag_id) values ('".$course_code."', '".$instructor."', ".$bookbag_id.")") or die( $db_session->lastErrorMsg() );
-	$db_session->close();
-*/
 
 	return true;
 }
@@ -37,7 +23,6 @@ function retrieve_reserve_list($file_store)
 	include('JSON.php');
 
 	// First- create db connection and query, then execute
-	//PHP5.2 way
 	$db_session = new PDO('sqlite:reserves.db');
 	$query = "SELECT * from reserve";
 
@@ -45,28 +30,11 @@ function retrieve_reserve_list($file_store)
 	$object_set = '';
 
 	foreach ($db_session->query($query) as $row) {
-	/*
-	//PHP5.3 way
-
-	$db_session = new SQLite3('reserves.db');
-	$query = "SELECT * from reserve";
-	$results = $db_session->query($query) or die( $db_session->lastErrorMsg() );
-	
-	$set_count = 0;
-	$object_set = '';
-
-	// Iterate over the results and do stuff with them
-	while ($row = $results->fetchArray())
-	{
-*/
 		//add to object set
-		//$object_set[$set_count] = array ('bookbag_id'=>$row["bookbag_id"], 'course_code'=>$row["course_code"], 'instructor'=>$row["instructor"]);
 		$object_set[$set_count] = $row;
 		$set_count++;
 		
 	}
-	
-//	$db_session->close();
 	
 	$wrapper = array('identifier'=>'bookbag_id', 'label'=>'course_code', 'items'=>$object_set);
 	$json = new Services_JSON();
@@ -123,31 +91,7 @@ function check_links()
 	echo "Done removing dead links... " . $count . " were removed.";
 }
 
-function test()
-{
-	echo "Going to test...";
-	if (generate_reserve("106", "COSC 2006", "Barry Adams"))
-	{
-		echo 'Test success';
-	}
-	else
-	{
-		echo "generate reserve test failed";
-	}
-//	generate_reserve("236", "COSC 1046", "Abou Rabia", $file_store) or die("generate reserve test failed");
-//	generate_reserve("145", "PSYC 2406", "Someone", $file_store) or die("generate reserve test failed");
-//	generate_reserve("167", "MATH 1046", "Calculor", $file_store) or die("generate reserve test failed");
-//	generate_reserve("436", "ENGL 1005", "English Prof", $file_store) or die("generate reserve test failed");
-}
-
-/*if (isset($_POST['coursecode'], $_POST['instructor'], $_POST['bookbagid']))
-{
-	$course_code = $_POST['coursecode'];
-	$instructor = $_POST['instructor'];
-	$bookbag_id = $_POST['bookbagid'];
-	generate_reserve($bookbag_id,$course_code,$instructor,$file_store);
-	echo "success";
-}*/
+//Handle calls to this script
 
 if ($_GET['mode'] == 'get')
 	retrieve_reserve_list($file_store);
